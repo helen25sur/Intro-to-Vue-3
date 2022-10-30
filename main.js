1e242cddbc129316973eeb39f6fbfd2b69d143e7
Elena Surilova <elen21surilova@gmail.com>
2022-10-30 21:50:28 +0200

    lesson 5 from VueMastery

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ const app = Vue.createApp({
       image: './assets/images/socks_green.jpg',
       // url: 'https://www.linkedin.com/in/olena-surilova-8aa66b122/',
       inventory: 100,
-      onSale: true,
+      // onSale: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' },
+      ],
+      // sizes: ['S', 'M', 'L', 'XL', 'XXl']
     }
   }
 })
