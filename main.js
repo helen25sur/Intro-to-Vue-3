eb145a442b29618d82e14cdab703b868427b9327
Elena Surilova <elen21surilova@gmail.com>
2022-10-30 21:30:10 +0200

    lesson 3 from VueMastery

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
-      descr: 'A sock is a piece of clothing worn on the feet and often covering the ankle or some part of the calf.'
+      descr: 'A sock is a piece of clothing worn on the feet and often covering the ankle or some part of the calf.',
+      image: './assets/images/socks_green.jpg',
+      url: 'https://www.linkedin.com/in/olena-surilova-8aa66b122/'
     }
   }
 })
