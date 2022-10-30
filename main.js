980c5bcc02f52af6ff2b05ba2ded0dc5b2c360c5
Elena Surilova <elen21surilova@gmail.com>
2022-10-30 22:02:24 +0200

    lesson 6 from VueMastery

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: 'Socks',
       // descr: 'A sock is a piece of clothing worn on the feet and often covering the ankle or some part of the calf.',
       image: './assets/images/socks_green.jpg',
@@ -9,10 +10,21 @@ const app = Vue.createApp({
       // onSale: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green' },
-        { id: 2235, color: 'blue' },
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
       ],
       // sizes: ['S', 'M', 'L', 'XL', 'XXl']
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+    removeFromCart() {
+      if (this.cart > 0) this.cart -= 1
     }
   }
 })
