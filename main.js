70d1929de9307e09a56d59bfe18ca559ddb69154
Elena Surilova <elen21surilova@gmail.com>
2022-10-30 21:14:57 +0200

    lesson 2 from VueMastery

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      descr: 'A sock is a piece of clothing worn on the feet and often covering the ankle or some part of the calf.'
+    }
+  }
+})
